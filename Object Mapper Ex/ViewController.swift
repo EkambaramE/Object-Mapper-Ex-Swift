eb11a9706b83983792f77b36314ab06be706db22
Ekambaram E <eekambaram90@gamil.com>
2016-03-31 15:02:20 +0530

    Initial Commit

---
+++ Object Mapper Ex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Object Mapper Ex
+//
+//  Created by EkambaramE on 31/03/16.
+//  Copyright © 2016 EkambaramE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
